c8a365fb5ba042a3e38191a60d93946e140e28a5
Juraj Michalek <juraj.michalek@sinusgear.com>
2013-12-21 15:55:49 +0100

    display message of current operation status, new button

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,12 @@
 var db;
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+phonecatApp.controller('PhoneListCtrl', function ($scope, $timeout) {
     $scope.records = [];
     $scope.searchKey = "";
     $scope.searchValue = "";
+    $scope.isSearchInProgress = false;
+    $scope.isResultEmpty = true;
 
     $scope.initialize =  function() {
         document.addEventListener('deviceready', $scope.onDeviceReady, false);
@@ -19,39 +21,51 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
                 {'k': res.rows.item(i).record_key,
                     'v': res.rows.item(i).record_descr});
         }
-
+        $scope.isResultEmpty = ($scope.records.length == 0);
+        $scope.isSearchInProgress = false;
     };
 
     $scope.search = function(keyQuery, valueQuery) {
-        db.transaction(function(tx) {
+        $scope.isSearchInProgress = true;
+        $scope.isResultEmpty = false;
+        $scope.records = [];
+        $timeout(function() {
+            db.transaction(function(tx) {
 
-            if ((keyQuery == null) || (keyQuery == "")) {
-                keyQuery = "%";
-            }
-
-            if (valueQuery == null) {
-                valueQuery = "";
-            }
-
-            keyQuery = keyQuery.replace(/\./g,"_");
-            valueQuery = valueQuery.replace(/\./g,"_");
-
-            keyQuery = keyQuery.replace("+","%");
-            valueQuery = valueQuery.replace("+","%");
-
-            if (valueQuery.indexOf("%") == -1) {
-                if (valueQuery == "") {
-                    valueQuery = "%";
-                } else {
-                    valueQuery = "%" + valueQuery + "%";
+                if ((keyQuery == null) || (keyQuery == "")) {
+                    keyQuery = "%";
                 }
-            }
 
-            var queryString = "select record_key, record_descr from dictionary where record_key LIKE ? and record_descr like ? limit 50;";
+                if (valueQuery == null) {
+                    valueQuery = "";
+                }
 
-            tx.executeSql(queryString, [keyQuery, valueQuery], $scope.processSearchResults);
-        });
+                keyQuery = keyQuery.replace(/\./g,"_");
+                valueQuery = valueQuery.replace(/\./g,"_");
 
+                keyQuery = keyQuery.replace("+","%");
+                valueQuery = valueQuery.replace("+","%");
+
+                if (valueQuery.indexOf("%") == -1) {
+                    if (valueQuery == "") {
+                        valueQuery = "%";
+                    } else {
+                        valueQuery = "%" + valueQuery + "%";
+                    }
+                }
+
+                var queryString = "select record_key, record_descr from dictionary where record_key LIKE ? and record_descr like ? limit 50;";
+
+                tx.executeSql(queryString, [keyQuery, valueQuery], $scope.processSearchResults);
+            })});
+
+    };
+
+    $scope.onSearchClear = function(event) {
+        $scope.searchKey = "";
+        $scope.searchValue = "";
+        $scope.records = [];
+        $scope.isResultEmpty = true;
     };
 
     $scope.onDeviceReady = function() {
